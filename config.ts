2b81809d71cf84243a1052400f4e20fc74b9ff01
abubekerethionyx <abubeker@ethionyx.com>
2026-01-30 23:31:03 +0300

    chore: Switch API base URL to production endpoint and delete `dist.zip`.

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-// export const BASE_URL = 'https://globalpathnewapi.peakstartgc.com';
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://globalpathnewapi.peakstartgc.com';
+// export const BASE_URL = 'http://localhost:5000';
 export const API_URL = `${BASE_URL}/api/v1`;
